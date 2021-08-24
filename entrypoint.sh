af99b5d6eb332495e9dd8d3c148004cdcc604bee
Ben Reynolds <ben.reynolds@drizly.com>
2021-08-24 14:39:25 -0400

    Test

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,14 @@ set -e
 
 function check_version {
   VERSION=$1
+  echo "Checking version: $VERSION"
   ! curl -s ${GITHUB_API_URL}/repos/${GITHUB_REPOSITORY}/git/matching-refs/tags | jq -e --arg VERSION "$VERSION" '.[] | select(.ref | endswith($VERSION))'
 
   if [ $? -eq 0 ]; then
+    echo "Version $VERSION is new!"
     echo "::set-output name=new-version::true"
   else
+    echo "Version $VERSION is already tagged on this repository!"
     echo "::set-output name=new-version::false"
   fi
 }
